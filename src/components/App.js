90d1840149b23d1f7233bbb94b66b94b107356ea
Anton Yarosh <on.yaroshanton@gmail.com>
2020-12-10 16:19:50 +0200

    add adding contact in localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,29 @@ export default class App extends Component {
             { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
             { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
             { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ],
         filter: ''
     }
+
+    componentDidMount() {
+        const persistedTasks = localStorage.getItem('contacts');
+
+        if (persistedTasks) {
+            this.setState({
+                contacts: JSON.parse(persistedTasks)
+            })
+        }
+    }
+
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.contacts !== this.state.contacts) {
+            localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+        }
+    }
+
+
 
     addContact = ({ name, number }) => {
         const contact = {
